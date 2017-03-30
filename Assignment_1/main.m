dee49c8aaecc112ebfc9c5b5ac8c71ee2420afb6
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-30 19:02:10 +1100

    Initial Commit

---
+++ Assignment_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment_1
+//
+//  Created by Vitaly Kuzenkov on 30/3/17.
+//  Copyright © 2017 Vitaly Kuzenkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
